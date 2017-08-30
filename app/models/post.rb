d2d3f2712f1ddae473f02f3cf000dbb4c0210305
Alex Beeken <aeb242@gmail.com>
2017-08-30 12:37:30 -0700

    add missing image and size news for responsive

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   def display_time
     created_at.strftime('%B %d %Y')
   end
+
+  def display_image
+    return thumbnail if Rails.application.assets.find_asset(thumbnail)
+    'missing.jpg'
+  end
 end
