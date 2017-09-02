dc45bd6a9b106b5905a5dec638abfba7abc80520
Alex Beeken <aeb242@gmail.com>
2017-09-01 17:21:41 -0700

    add video preview images and edit page for videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,4 +6,9 @@ class VideosController < ApplicationController
   def index
     @videos = Video.order(post_date: :desc).to_a
   end
+
+  def edit
+    @videos = Video.all
+    @video = Video.find(params[:id])
+  end
 end
