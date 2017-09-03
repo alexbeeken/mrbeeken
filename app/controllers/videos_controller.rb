6197908d3d979734bf76f5c0d653d78f88534943
Alex Beeken <aeb242@gmail.com>
2017-09-02 17:23:12 -0700

    remove summary from model and all edit routes

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,14 +1,5 @@
 class VideosController < ApplicationController
-  def show
-    @video = Video.find(params[:id])
-  end
-
   def index
     @videos = Video.order(post_date: :desc).to_a
-  end
-
-  def edit
-    @videos = Video.all
-    @video = Video.find(params[:id])
   end
 end
--- db/migrate/20170831053524_create_videos.rb
+++ db/migrate/20170831053524_create_videos.rb
@@ -2,7 +2,6 @@ class CreateVideos < ActiveRecord::Migration[5.1]
   def change
     create_table :videos do |t|
       t.string :title
-      t.string :summary
       t.string :url
       t.string :thumbnail
       t.datetime :post_date
